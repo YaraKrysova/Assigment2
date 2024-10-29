303fa9a1785b00a48b3dbd41a88eae7bb4785b62
Iaroslava Krysova <yaroslavakrysova@iaroslavas-mbp.home>
2024-10-29 12:00:10 -0400

    Added init

--- plantInformation.py
+++ plantInformation.py
@@ -6,4 +6,8 @@ class PlantInfo:
     growthRate: 0.0     #how many centimetrs plant grows in 1 day
     numberOfDays: 0  #how many days it'd take to the fastest-growing plant to become bigger than 2nd
 
+    def __init__(self, name="", startingHeight=0.0, growthRate=0.0):
+        self.name = name
+        self.startingHeight = startingHeight
+        self.growthRate = growthRate
 
--- program.py
+++ program.py
@@ -2,7 +2,6 @@ from plantInformation import PlantInfo
 
 class Program:
     plant1 = PlantInfo()
-
     plant2 = PlantInfo()
     
     print("============ PLANT GROWTH COMPARISON PROGRAM ============")
@@ -12,7 +11,57 @@ class Program:
     plant1.startingHeight = int(input("Starting height (cm): "))
     plant1.growthRate = int(input("Daily growth rate: "))
 
+    print("Second plant...")
     plant2.name = input("Name: ")
     plant2.startingHeight = int(input("Starting height (cm): "))
     plant2.growthRate = int(input("Daily growth rate: "))
+
+
+    def displayMenu(self):
+        print("**Menu**")
+
+        print("1) Display plant information")
+        print("2) Projected growth over time")
+        print("3) Project day when plant with fastest growth rate is tallest")
+        print("4) Change plant information")
+        print("5) Quit program")
+
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            print(f"{plant1.name} is {plant1.startingHeight} cm tall and has a daily growth rate of {plant1.growthRate} cm per day")
+            print(f"{plant2.name} is {plant2.startingHeight} cm tall and has a daily growth rate of {plant2.growthRate} cm per day")
+
+                
+        elif choice == 2:
+            days = int(input("How many days of growth? "))
+            x = 1
+            while x < days:
+                heightOnXDay1 = plant1.startingHeight + plant1.growthRate
+                heightOnXDay2 = plant2.startingHeight + plant2.growthRate
+                print(f"{x}: {plant1.name} is {heightOnXDay1} and {plant2.name} is {heightOnXDay2}")
+                x = x + 1
+        
+        elif choice == 4:
+            print("First plant...")
+            plant1.name = input("Name: ")
+            plant1.startingHeight = int(input("Starting height (cm): "))
+            plant1.growthRate = int(input("Daily growth rate: "))
+
+            print("Second plant...")
+            plant2.name = input("Name: ")
+            plant2.startingHeight = int(input("Starting height (cm): "))
+            plant2.growthRate = int(input("Daily growth rate: "))
+
+        elif choice == 5:
+            break
+
+
+
+
+
+
+
+
+
 
